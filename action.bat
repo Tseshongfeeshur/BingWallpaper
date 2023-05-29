68670221617b1377aea51693616e6b2283c3f8ea
Ryan璃黯 <99335871+Tseshongfeeshur@users.noreply.github.com>
2023-05-29 14:11:12 +0800

    Add files via upload

---
+++ action.bat
@@ -0,0 +1,24 @@
+set curl=
+set curl=curl\bin\curl.exe
+%curl% "https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN" | jq .images | jq .[] >%tmp%\wallpaper.json
+jq .urlbase %tmp%\wallpaper.json -r >%tmp%\tmp.txt
+set url=
+set /p url=<%tmp%\tmp.txt
+jq .copyright %tmp%\wallpaper.json -r >%tmp%\tmp.txt
+set describe
+set /p describe=<%tmp%\tmp.txt
+jq .copyrightlink %tmp%\wallpaper.json -r >%tmp%\tmp.txt
+set search=
+set /p search=<%tmp%\tmp.txt
+jq .title %tmp%\wallpaper.json -r >%tmp%\tmp.txt
+set title=
+set /p title=<%tmp%\tmp.txt
+echo "https://www.bing.com%url%" >%tmp%\tmp.txt
+echo "%search%" >>%tmp%\tmp.txt
+md output
+echo {"Title":"%title%","Describe":"%describe%","UrlIn4k":"https://www.bing.com%url%_UHD.jpg","UrlIn1080p":"https://www.bing.com%url%_1920x1080.jpg","UrlIn720p":"https://www.bing.com%url%_1280x720.jpg","Search":"%search%"} |  jq . >output\all.json
+curl "https://www.bing.com%url%_UHD.jpg" -o output\4k.jpg
+curl "https://www.bing.com%url%_1920x1080.jpg" -o output\1080p.jpg
+curl "https://www.bing.com%url%_1280x720.jpg" -o output\720p.jpg
+del %tmp%\wallpaper.json
+del %tmp%\tmp.txt
