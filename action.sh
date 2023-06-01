4169b72df98152fd1939fba065787ad24171c3e1
Ryan璃黯 <99335871+Tseshongfeeshur@users.noreply.github.com>
2023-06-01 13:52:20 +0800

    Update action.sh

--- action.sh
+++ action.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 mkdir mydir
-curl "https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN" | jq .images | jq .[] >mydir/wallpaper.json
-jq .urlbase mydir/wallpaper.json -r >mydir/tmp.txt
-read -r url<mydir/tmp.txt
-jq .copyright mydir/wallpaper.json -r >mydir/tmp.txt
-read -r describe<mydir/tmp.txt
-jq .copyrightlink mydir/wallpaper.json -r >mydir/tmp.txt
-read -r search<mydir/tmp.txt
-jq .title mydir/wallpaper.json -r >mydir/tmp.txt
-read -r title<mydir/tmp.txt
+curl "https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN" | jq .images | jq .[] > mydir/wallpaper.json
+jq .urlbase mydir/wallpaper.json -r > mydir/tmp.txt
+read -r url < mydir/tmp.txt
+jq .copyright mydir/wallpaper.json -r > mydir/tmp.txt
+read -r describe < mydir/tmp.txt
+jq .copyrightlink mydir/wallpaper.json -r > mydir/tmp.txt
+read -r search < mydir/tmp.txt
+jq .title mydir/wallpaper.json -r > mydir/tmp.txt
+read -r title < mydir/tmp.txt
 mkdir output
-echo {"Title":"$title","Describe":"$describe","UrlIn4k":"https://www.bing.com$url_UHD.jpg","UrlIn1080p":"https://www.bing.com$url_1920x1080.jpg","UrlIn720p":"https://www.bing.com$url_1280x720.jpg","Search":"$search"} | jq . >output/all.json
-curl "https://www.bing.com$url_UHD.jpg" -o output/4k.jpg
-curl "https://www.bing.com$url_1920x1080.jpg" -o output/1080p.jpg
-curl "https://www.bing.com$url_1280x720.jpg" -o output/720p.jpg
+echo "{\"Title\":\"${title}\",\"Describe\":\"${describe}\",\"UrlIn4k\":\"https://www.bing.com${url}_UHD.jpg\",\"UrlIn1080p\":\"https://www.bing.com${url}_1920x1080.jpg\",\"UrlIn720p\":\"https://www.bing.com${url}_1280x720.jpg\",\"Search\":\"${search}\"}" | jq . > output/all.json
+curl "https://www.bing.com${url}_UHD.jpg" -o output/4k.jpg
+curl "https://www.bing.com${url}_1920x1080.jpg" -o output/1080p.jpg
+curl "https://www.bing.com${url}_1280x720.jpg" -o output/720p.jpg
 mkdir temp
 cd temp
 git init
